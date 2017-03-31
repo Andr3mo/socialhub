57381f061fdae44cf99da034652209ea47158dc3
Anderson <andersonpirespereira@hotmail.com>
2017-03-31 00:25:56 -0500

    Changed the ticket model to have an image and added a default image file to the assests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330192943) do
+ActiveRecord::Schema.define(version: 20170331052133) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "name"
@@ -19,9 +19,13 @@ ActiveRecord::Schema.define(version: 20170330192943) do
     t.decimal  "price"
     t.integer  "stock_quantity"
     t.string   "status"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
