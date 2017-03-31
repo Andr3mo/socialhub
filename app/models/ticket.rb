547b0b2af57877c3ca3016b640e34aaa74968bf7
Anderson <andersonpirespereira@hotmail.com>
2017-03-31 00:52:16 -0500

    Added presence validation to the ticket model for some attributes

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 # rubocop:disable LineLength
 class Ticket < ApplicationRecord
+  validates :name, :image, :code, :price, :stock_quantity, presence: true
   belongs_to :user
 
   has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '150x150>' }, default_url: '/system/tickets/images/missing.png'
