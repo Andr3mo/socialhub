30c4e11b14f7ac683b02955f91bd8021502fbc57
Anderson <andersonpirespereira@hotmail.com>
2017-04-02 00:19:41 -0500

    Changed the views to accepted input to the new user details required and changed the method in the application controller to permit the params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |user| user.permit(:first_name, :last_name, :city, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |user| user.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |user| user.permit(:first_name, :last_name, :address, :city, :country, :postal_code, :email, :password, :current_password, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:first_name, :last_name, :city, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_in) { |user| user.permit(:email, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:first_name, :last_name, :address, :city, :country, :postal_code, :email, :password, :current_password, :remember_me) }
   end
 end
