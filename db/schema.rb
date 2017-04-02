23a53c339566629941ad3d89ec1a308cae00bb9e
Anderson <andersonpirespereira@hotmail.com>
2017-04-02 17:37:48 -0500

    Added the Province model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402214723) do
+ActiveRecord::Schema.define(version: 20170402223534) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 20170402214723) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string   "name"
+    t.string   "abbreviation"
+    t.decimal  "pst"
+    t.decimal  "gst"
+    t.decimal  "hst"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "tickets", force: :cascade do |t|
