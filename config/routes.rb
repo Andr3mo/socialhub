7e33de7c92232fafc54aa3bc6234c9b08cf0faed
Anderson <andersonpirespereira@hotmail.com>
2017-04-02 03:21:55 -0500

    Created a user controller with a show view where the user can view its information and tickets and added a route to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   resources :tickets
+  resources :users
 
   get '/tickets/search/:q', to: 'tickets#search', as: :search
 
