5bcf01eb11a98817e607426132555f3c974ae36f
Anderson <andersonpirespereira@hotmail.com>
2017-04-02 16:45:22 -0500

    Created the category model and added the relationships between category and tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ApplicationRecord
   searchkick
   validates :name, :image, :code, :price, :stock_quantity, presence: true
   belongs_to :user
+  belongs_to :category
 
   has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '150x150>' }, default_url: '/system/tickets/images/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
