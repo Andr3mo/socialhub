dacea75d4376e98b874c7769b2118104ba850b76
Anderson <andersonpirespereira@hotmail.com>
2017-04-01 22:50:36 -0500

    Added a migration to add details to the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331052133) do
+ActiveRecord::Schema.define(version: 20170402034238) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20170331052133) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "address"
+    t.string   "city"
+    t.string   "country"
+    t.string   "postal_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
