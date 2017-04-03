2c315c3e09430b5d3dfb566c1edbe99e4eea266a
Anderson <andersonpirespereira@hotmail.com>
2017-04-02 22:36:30 -0500

    Added the province to the create, edit and show user views after changing the controller to allow the province on devise views/actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :account_update, if: :devise_controller?
 
-  def sign_up
+  def account_update
     @provinces = Province.all
   end
 
@@ -13,6 +14,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:first_name, :last_name, :province_id, :city, :email, :password, :remember_me) }
     devise_parameter_sanitizer.permit(:sign_in) { |user| user.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:first_name, :last_name, :address, :city, :country, :postal_code, :email, :password, :current_password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:first_name, :last_name, :province_id, :address, :city, :country, :postal_code, :email, :password, :current_password, :remember_me) }
   end
 end
